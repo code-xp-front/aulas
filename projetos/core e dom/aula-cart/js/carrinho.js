122c1e8c199795ec738f30e2972a76ae0bb4731c
mendeseducosta <mendes.educosta@gmail.com>
2018-01-12 18:57:19 -0200

    hands on atualizado

--- projetos/core e dom/aula-cart/js/carrinho.js
+++ projetos/core e dom/aula-cart/js/carrinho.js
@@ -3,11 +3,11 @@ var produtos = [
     {//0
         codigo : 50,
         imagem : {
-            src     : '/img/camiseta.png',
+            src     : './img/camiseta.png',
             alt     : "texto alternativo",
             title   : "título da imagem"
         },
-        desricao    : 'Puma Golf Raglan Tech Polo Tee',
+        descricao    : 'Puma Golf Raglan Tech Polo Tee',
         cor         : 'Purple',
         preco       : 72,
         qtd         : 3
@@ -15,11 +15,11 @@ var produtos = [
     {//1
         codigo : 99,
         imagem : {
-            src     : '/img/camiseta.png',
+            src     : './img/camiseta.png',
             alt     : "texto alternativo",
             title   : "título da imagem"
         },
-        desricao    : 'Nike Mens Running Shoe',
+        descricao    : 'Nike Mens Running Shoe',
         cor         : 'White',
         preco       : 50,
         qtd         : 1
@@ -27,11 +27,11 @@ var produtos = [
     {//2
         codigo : 287,
         imagem : {
-            src     : '/img/camiseta.png',
+            src     : './img/camiseta.png',
             alt     : "texto alternativo",
             title   : "título da imagem"
         },
-        desricao    : 'DC Mens Axis Snowboard Jacket',
+        descricao    : 'DC Mens Axis Snowboard Jacket',
         cor         : 'White',
         preco       : 89.97,
         qtd         : 2
@@ -55,18 +55,22 @@ function listarProdutos(){
     var template = "";
     
     for(var i = 0; i < produtos.length; i++){
-        
+        console.log( produtos[i] );
         template += '<div class="product">';
         template +=     '<i class="fa fa-times-circle"></i>';
-        template +=     '<img src="img/camiseta.png" alt="t-shirt" class="product__img">';
-        template +=     '<span id="p1" class="product__name"></span>';
+        template +=     '<img src="' +produtos[i].imagem.src+ '" alt="t-shirt" class="product__img">';
+        template +=     '<span id="p1" class="product__name">'+produtos[i].descricao+'</span>';
         template +=     '<span id="c1" class="product__color">'+produtos[i].cor+'</span>';
         template +=     '<span id="s1" class="product__size"></span>';
-        template +=     '<span id="v1" class="product__value"></span>*<input type="number" name="" id=""> = <span>$216</span>';
+        template +=     '<span id="v1" class="product__value"><strong>R$ </strong>'+produtos[i].preco+'</span>*<input type="number"  value="'+produtos[i].qtd+'" name="" id=""> = <span> <strong>R$ </strong>'+(produtos[i].qtd * produtos[i].preco)+'</span>';
         template += '</div>';
     }
 
     document.getElementById("products").innerHTML = template;
+}
+
+function validaSexo(_id){
+    alert(_id);
 }
 
 
@@ -82,33 +86,6 @@ function listarProdutos(){
 
 
 
-
-
-
-
-
-
-// document.getElementById("p1").innerHTML = productInfo1.name;
-// document.getElementById("c1").innerHTML ='Color: '+productInfo1.color;
-// document.getElementById("v1").innerHTML = '$'+productInfo1.price;
-
-
-// var productInfo2 = {
-//     name:'Nike Mens Running Shoes',
-//     color: 'White',
-//     price: 119
-// };
-
-// document.getElementById("p2").innerHTML = productInfo2.name;
-// document.getElementById("c2").innerHTML ='Color: '+productInfo2.color;
-// document.getElementById("v2").innerHTML = '$'+productInfo2.price;
-
-// var productInfo3 = {
-//     name:'DC Mens Axis Snowboard Jacket',
-//     color: 'Blue',
-//     price: 89
-// };
-
-// document.getElementById("p3").innerHTML = productInfo3.name;
-// document.getElementById("c3").innerHTML ='Color: '+productInfo3.color;
-// document.getElementById("v3").innerHTML = '$'+productInfo3.price;
+/* function validaSexo(_id){
+    alert(_id);
+} */
