461d7fddc82347d6838919eb8caa2ce08a7d5385
mendeseducosta <mendes.educosta@gmail.com>
2018-01-12 21:59:55 -0200

    atualização da quantidade  atualização do handson

--- projetos/core e dom/aula-cart/js/carrinho.js
+++ projetos/core e dom/aula-cart/js/carrinho.js
@@ -66,7 +66,7 @@ function listarProdutos(){
         template +=     '<span id="p1" class="product__name">'+produtos[i].descricao+'</span>';
         template +=     '<span id="c1" class="product__color">'+produtos[i].cor+'</span>';
         template +=     '<span id="s1" class="product__size"></span>';
-        template +=     '<span id="v1" class="product__value"><strong>R$ </strong>'+produtos[i].preco+'</span>*<input type="number"  value="'+produtos[i].qtd+'" name="" id=""> = <span> <strong>R$ </strong>'+totalProduto+'</span>';
+        template +=     '<span id="v1" class="product__value"><strong>R$ </strong>'+produtos[i].preco+'</span>*<input type="number" onchange="atualizarQuantidade(this.id, this.value)" value="'+produtos[i].qtd+'" name="" id="'+produtos[i].codigo+'"> = <span id="total-'+produtos[i].codigo+'"> <strong>R$ </strong>'+totalProduto+'</span>';
         template += '</div>';
     }
 
@@ -75,23 +75,23 @@ function listarProdutos(){
     //  R$523
 }
 
-function validaSexo(_id){
-    alert(_id);
+function atualizarQuantidade(_codigo, _qtd){
+    
+    for (var index = 0; index < produtos.length; index++) {
+        
+        if( produtos[index].codigo == _codigo ){
+            //atualizar a quantidade
+            produtos[index].qtd = _qtd;
+            
+            //atualizar o total do produto
+            var totalProduto = produtos[index].qtd * produtos[index].preco;
+            var codProd = 'total-' + produtos[index].codigo;
+
+            document.getElementById(codProd).innerHTML = "<strong>R$ </strong>"+totalProduto.toFixed(2);
+            //atualizar o total geral
+            console.log(produtos[index]);
+            break;
+        }        
+
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function validaSexo(_id){
-    alert(_id);
-} */
