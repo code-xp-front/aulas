43eb86270993b6a470309dd01c2cb4c4be03bb4c
mendeseducosta <mendes.educosta@gmail.com>
2018-01-12 20:59:35 -0200

    Atualização v2

--- projetos/core e dom/aula-cart/js/carrinho.js
+++ projetos/core e dom/aula-cart/js/carrinho.js
@@ -53,21 +53,26 @@ var produtos = [
 function listarProdutos(){
  //Gerar o template através do array de objetos
     var template = "";
-    
+    var totalGeral = 0;
     
     for(var i = 0; i < produtos.length; i++){
-        console.log( produtos[i] );
+        
+        var totalProduto = produtos[i].qtd * produtos[i].preco;
+        totalGeral += totalProduto;
+
         template += '<div class="product">';
         template +=     '<i class="fa fa-times-circle"></i>';
         template +=     '<img src="' +produtos[i].imagem.src+ '" alt="t-shirt" class="product__img">';
         template +=     '<span id="p1" class="product__name">'+produtos[i].descricao+'</span>';
         template +=     '<span id="c1" class="product__color">'+produtos[i].cor+'</span>';
         template +=     '<span id="s1" class="product__size"></span>';
-        template +=     '<span id="v1" class="product__value"><strong>R$ </strong>'+produtos[i].preco+'</span>*<input type="number"  value="'+produtos[i].qtd+'" name="" id=""> = <span> <strong>R$ </strong>'+(produtos[i].qtd * produtos[i].preco)+'</span>';
+        template +=     '<span id="v1" class="product__value"><strong>R$ </strong>'+produtos[i].preco+'</span>*<input type="number"  value="'+produtos[i].qtd+'" name="" id=""> = <span> <strong>R$ </strong>'+totalProduto+'</span>';
         template += '</div>';
     }
 
     document.getElementById("products").innerHTML = template;
+    document.getElementById("total-geral").innerHTML = "Subtotal: <strong>R$" + totalGeral + "</strong>";
+    //  R$523
 }
 
 function validaSexo(_id){
