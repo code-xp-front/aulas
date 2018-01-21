657bfe1847bde7f05841caa0f31e29474c52a72f
Camila Belo <camilaibs@gmail.com>
2018-01-20 23:18:17 -0200

    Add files via upload

--- exemplos/dom-core/js/scripts.js
+++ exemplos/dom-core/js/scripts.js
@@ -4,38 +4,66 @@ function updateView(section) {
     var innerHTML = "";
 
     for (var index = 0; index < notes.length; index++) {
-        innerHTML += '<form class="note">'+
-                        '<button class="note__control" type="button" onclick="onRemoveClick(this.form.parentElement, ' + index + ')">' +
-                            '<i class="fa fa-times" aria-hidden="true"></i>' +
-                        '</button>' +
-                         '<h1 class="note__title">' + notes[index].title + '</h1>' +
-                         '<p class="note__body">' + notes[index].body + '</p>' +
-                     '</form>';
+        if (notes[index].editing) {
+            innerHTML += '<form class="note">'+
+                             '<input class="note__title" type="text" name="title" value="' + notes[index].title + '" placeholder="Título" />' +
+                             '<textarea class="note__body" name="body" rows="5" placeholder="Criar uma nota...">' + notes[index].body + '</textarea>' +
+                             '<button class="note__control" type="button" onclick="onDoneClick(this.form, this.form.parentElement, this.form.title, this.form.body, ' + index + ')">' +
+                                'Concluído' +
+                             '</button>' +
+                         '</form>';
+        } else {
+            innerHTML += '<form class="note" onclick="onEditClick(this.parentElement, ' + index + ')">'+
+                             '<button class="note__control" type="button" onclick="onRemoveClick(this.form.parentElement, ' + index + ')">' +
+                                 '<i class="fa fa-times" aria-hidden="true"></i>' +
+                             '</button>' +
+                             '<h1 class="note__title">' + notes[index].title + '</h1>' +
+                             '<p class="note__body">' + notes[index].body + '</p>' +
+                         '</form>';
+        }
     }
 
     section.innerHTML = innerHTML;
 }
 
-function onDoneClick(form, section, inputTitle, textAreaBody) {
-    // criar uma nota
-    var note = {
-        title: inputTitle.value,
-        body: textAreaBody.value
-    };
+function onDoneClick(form, section, inputTitle, textAreaBody, index) {
+    if (index === null) {
+        // criar uma nota
+        var note = {
+            title: inputTitle.value,
+            body: textAreaBody.value
+        };
 
-    // adicionar a nota
-    notes.push(note);
+        // adicionarz a nota
+        notes.push(note);
 
-    // atualizar a tela
-    updateView(section);
+        // atualizar a tela
+        updateView(section);
 
-    //limpar o formulario
-    form.reset();
+        //limpar o formulario
+        form.reset();
+    } else {
+        // alterar a nota
+        notes[index].title = inputTitle.value;
+        notes[index].body = textAreaBody.value;
+        notes[index].editing = false;
+
+        // atualizar a tela
+        updateView(section);
+    }
 }
 
 function onRemoveClick(section, index) {
     // remover a nota
     notes.splice(index, 1);
+
+    // atualizar a tela
+    updateView(section);
+}
+
+function onEditClick(section, index) {
+    // adicionar atributo editando
+    notes[index].editing = true;
 
     // atualizar a tela
     updateView(section);
